ead0cf226a674ef941148ab6fd58ae1ac312827d
Alexandra Marks <amarks93@gmail.com>
2021-10-03 22:15:36 -0400

    still debugging deployment

---
+++ build/15b15ae9bb4365213ca0.js
@@ -0,0 +1,33 @@
+import Phaser, { Game } from 'phaser';
+import PhaserMatterCollisionPlugin from 'phaser-matter-collision-plugin';
+import TitleScene from './scenes/TitleScene.js';
+import MainScene from './scenes/MainScene.js';
+import EndScene from './scenes/EndScene.js';
+var config = {
+  width: 256,
+  height: 256,
+  backgroundColor: '#723d46',
+  type: Phaser.AUTO,
+  parent: 'being-a-grown-up',
+  scene: [TitleScene, MainScene, EndScene],
+  scale: {
+    zoom: 2
+  },
+  physics: {
+    "default": 'matter',
+    matter: {
+      debug: false,
+      gravity: {
+        y: 0
+      }
+    }
+  },
+  plugins: {
+    scene: [{
+      plugin: PhaserMatterCollisionPlugin,
+      key: 'matterCollision',
+      mapping: 'matterCollision'
+    }]
+  }
+};
+new Game(config);
